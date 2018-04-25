f92af2af555a643ae5de092c3dc72b81a34f7692
brodderick rodriguez <bcr@auburn.edu>
2018-04-24 20:16:18 -0500

    t c

---
+++ main/transactionsubsys/AutomaticBillPay.java
@@ -0,0 +1,17 @@
+package main.transactionsubsys;
+import main.repositorysys.Bill;
+
+
+public class AutomaticBillPay {
+    //private Bill billsOnAutoPay[];
+    private int currentState;
+    // private Form form;
+
+//    public void AutomaticBillPay() {
+//
+//    }
+
+//   public void cancelAutomaticBill() {
+//    }
+
+}
