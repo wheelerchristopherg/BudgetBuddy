df0a53f2267a080fe32dabdc744c9285825c29ab
cgwheeler96 <wheelerchristopherg@gmail.com>
2018-04-24 20:49:17 -0500

    work on UI

--- main/Main.java
+++ main/Main.java
@@ -1,13 +1,10 @@
-import userinterface.Window;
-import main.repositorysys.BillPayReminder;
+package main;
+
+import main.userinterface.Window;
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Program startup check
-
-
         SwingUtilities.invokeLater(
             new Runnable() {
                 public void run() {
--- main/userinterface/Form.java
+++ main/userinterface/Form.java
@@ -1,5 +1,13 @@
 package main.userinterface;
 
-public class Form {
+import javax.swing.JPanel;
+
+public class Form extends JPanel{
+    
+    private parent;
+    
+    public Form(Window parent) {
+        this.parent = parent;
+    }
     
 }
