8051d98a8b097b60db62d0e32225ad04043e6016
cgwheeler96 <wheelerchristopherg@gmail.com>
2018-04-25 01:42:54 -0500

    added helper functions for making GUIs

--- main/userinterface/MainForm.java
+++ main/userinterface/MainForm.java
@@ -1,23 +1,42 @@
 package main.userinterface;
 
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
 
 public class MainForm extends Form {
     
     public MainForm(Window parent) {
         super(parent);
-        JButton testButton1 = new JButton("Test button 1");
-        JButton testButton2 = new JButton("Test button 2"); 
-        testButton1.addActionListener(this);
-        testButton2.addActionListener(this);
         
-        this.add(testButton1);
-        this.add(testButton2);
+        setGridLayout(9, 2);
         
+        addPlaceholders(2);
+        addButton("button1","Print text");
+        addTextField("input1", "box 1");
+        addPlaceholders(2);
+        addButton("button2","Print text");
+        addTextField("input2", "box 2");
+        addPlaceholders(2);
+        addButton("button3","Print text");
+        addTextField("input3", "box 3");
+        addPlaceholders(6);
     }
     
     public void actionPerformed(ActionEvent event) {
+        String name = buttonPressed(event);
+        String output = "";
+        switch (name) {
+            case "button1":
+                output = getTextFromInput("input1");
+                break;
+            case "button2":
+                output = getTextFromInput("input2");
+                break;
+            case "button3":
+                output = getTextFromInput("input3");
+                break;
+            
+        }
         
+        System.out.println(output);
     }
 }
