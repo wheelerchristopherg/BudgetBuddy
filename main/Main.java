9dab05ee025c4a3da02abb7138abb01e41d2866f
TCB0027 <TCB0027@auburn.edu>
2018-04-24 20:20:56 -0500

    Merge branch 'master' of https://github.com/wheelerchristopherg/BudgetBuddy
    # Conflicts:
    #	Main.java

--- main/Main.java
+++ main/Main.java
@@ -4,6 +4,10 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Program startup check
+
+
         SwingUtilities.invokeLater(
             new Runnable() {
                 public void run() {
--- main/userinterface/Form.java
+++ main/userinterface/Form.java
@@ -1,3 +1,5 @@
+package main.userinterface;
+
 public class Form {
     
 }
