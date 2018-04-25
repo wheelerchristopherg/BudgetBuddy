8d6bc192a50c56260e0c41844c71b8c4ef4e2655
brodderick rodriguez <bcr@auburn.edu>
2018-04-24 21:21:12 -0500

    re commit

---
+++ main/transactionsubsys/Bill.java
@@ -0,0 +1,14 @@
+package main.repositorysys;
+
+import java.util.*;
+
+public class Bill {
+
+  String name;
+  String type;
+  double value;
+  Date dueDate;
+  boolean recurrance;
+//  Account desiredAccount;
+
+}
