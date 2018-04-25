bb709c34ce7a98c65db779c234d15497c0fb24cd
TCB0027 <TCB0027@auburn.edu>
2018-04-24 19:35:45 -0500

    BillPayReminder

--- repositorysys/BillPayReminder.java
+++ repositorysys/BillPayReminder.java
@@ -1,9 +1,31 @@
 package repositorysys;
 
+import java.util.Date;
+
 public class BillPayReminder {
 
-  double amount;
-  double interestRate;
-  int reminderType;
-  
+    private String name;
+    private double amount;
+    private Date reminderDate; // MM-dd-yyyy format
+
+
+
+    public BillPayReminder(String name, double amount, Date reminderDate) {
+        this.name = name;
+        this.amount = amount;
+        this.reminderDate = reminderDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Date getReminderDate() {
+        return reminderDate;
+    }
+    
 }
