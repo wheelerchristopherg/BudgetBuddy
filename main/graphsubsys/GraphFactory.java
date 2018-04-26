758a2bcd2de8b8f0b122c3558af45aac1bffab2e
cgwheeler96 <wheelerchristopherg@gmail.com>
2018-04-25 20:15:24 -0500

    added line graph factory

--- main/graphsubsys/GraphFactory.java
+++ main/graphsubsys/GraphFactory.java
@@ -1,9 +1,13 @@
 package main.graphsubsys;
 
-
+import java.awt.geom.Point2D;
 
 public class GraphFactory {
     public static Graph createLineGraph(double[] x, double[] y) {
+        Graph lineGraph = new Graph("lineGraph", double[] x, double[] y);
+    }
+    
+    public static Graph createBarGraph() {
         
     }
 }
