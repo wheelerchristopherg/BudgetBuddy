10f5824bbf74b61396c51ab766c9efbbdf370102
cgwheeler96 <wheelerchristopherg@gmail.com>
2018-04-26 13:43:45 -0500

    line graph works

--- main/graphsubsys/Graph.java
+++ main/graphsubsys/Graph.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 import java.awt.geom.Path2D;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -23,20 +24,27 @@ public class Graph extends JPanel {
     
     private double[] xPointData;
     private double[] yPointData;
+    private double maxX;
+    private double minX;
+    private double maxY;
+    private double minY;
     private HashMap<String, Double> labelToValue;
     
     private double maxHeight;
     
     private Graph() {
-        this.setSize(300, 300);
+        this.setSize(250, 250);
     }
     
     protected Graph(int type, double[] x, double[] y) {
         this();
         this.type = type;
         
+        
         xPointData = x.clone();
         yPointData = y.clone();
+        
+        setMinMax(xPointData, yPointData);
     }
     
     protected Graph(double maxHeight, double[] stepHeights) {
@@ -45,6 +53,7 @@ public class Graph extends JPanel {
         this.maxHeight = maxHeight;
         
         yPointData = stepHeights.clone();
+        setMinMax(yPointData);
     }
     
     protected Graph(double[] percentages, String[] labels) {
@@ -55,6 +64,44 @@ public class Graph extends JPanel {
         for (int i = 0; i < percentages.length; i++) {
             labelToValue.put(labels[i], percentages[i]);
         }
+    }
+    
+    private void setMinMax(double[] x, double[] y) {
+        minX = x[0];
+        maxX = x[x.length-1];
+        minY = y[0];
+        maxY = y[0];
+        for (int i = 1; i < y.length; i++) {
+            if (y[i] > maxY) {
+                maxY = y[i];
+            }
+            if (y[i] < minY) {
+                minY = y[i];
+            }
+        }
+        
+        System.out.println("maxX: " + maxX);
+        System.out.println("minX: " + minX);
+        System.out.println("maxY: " + maxY);
+        System.out.println("minY: " + minY);
+    }
+    
+    private void setMinMax(double[] y) {
+        minY = y[0];
+        maxY = y[0];
+        for (int i = 1; i < y.length; i++) {
+            if (y[i] > maxY) {
+                maxY = y[i];
+            }
+            if (y[i] < minY) {
+                minY = y[i];
+            }
+        }
+        
+        System.out.println("maxX: " + maxX);
+        System.out.println("minX: " + minX);
+        System.out.println("maxY: " + maxY);
+        System.out.println("minY: " + minY);
     }
     
     protected void addLabels(String x, String y) {
@@ -80,8 +127,37 @@ public class Graph extends JPanel {
         }
     }
     
+    private double[] convertPoint(double x, double y) {
+        double convertedX = (((x-minX) / (maxX - minX)) * 200.0) + 10.0;
+        double convertedY = (((y-minY) / (maxY - minY)) * 200.0);
+        convertedY = 210 - convertedY;
+        double[] point = new double[2];
+        point[0] = convertedX;
+        point[1] = convertedY;
+        
+        System.out.println("x: " + convertedX);
+        System.out.println("y: " + convertedY);
+        System.out.println();
+        
+        return point;
+    }
+    
     private void drawLineGraph(Graphics2D g) {
         drawAxes(g);
+        
+        double[] point = convertPoint(xPointData[0], yPointData[0]);
+        Path2D.Double path = new Path2D.Double();
+        
+        path.moveTo(point[0], point[1]);
+        for (int i = 0; i < xPointData.length; i++) {
+            point = convertPoint(xPointData[i], yPointData[i]);
+            path.lineTo(point[0], point[1]);
+        }
+        System.out.println(path);
+        
+        g.setColor(Color.RED);
+        g.draw(path);
+        
     }
     
     private void drawBarGraph(Graphics2D g) {
@@ -97,11 +173,12 @@ public class Graph extends JPanel {
     }
     
     private void drawAxes(Graphics2D g) {
-        Path2D axes = new Path2D.Double();
-        axes.moveTo(50.0, 50.0);
-        axes.lineTo(50.0, 250.0);
-        axes.lineTo(250.0, 250.0);
+        Path2D.Double axes = new Path2D.Double();
+        axes.moveTo(10.0, 10.0);
+        axes.lineTo(10.0, 210.0);
+        axes.lineTo(210.0, 210.0);
         
+        g.setColor(Color.BLACK);
         g.draw(axes);
     }
 }
