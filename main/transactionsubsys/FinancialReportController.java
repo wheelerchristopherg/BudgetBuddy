764630b864a3019fdc619de3f2dbf5044b99b298
RSMcW13 <38706639+RSMcW13@users.noreply.github.com>
2018-04-26 17:25:36 -0500

    BudgetController uses JOptionPanes to prompt for input

--- main/transactionsubsys/FinancialReportController.java
+++ main/transactionsubsys/FinancialReportController.java
@@ -2,8 +2,10 @@
 
 public class FinancialReportController {
 
-    public void FinancialReportController() {
-
+    public void FinancialReportController(Form zForm) {
+        ans = JOptionPane.showInputDialog(zForm, "Would you like to use an Account or a Budget?", "Request",1,4,null,["Account","Budget"],null);
+        // if ans = 0, display list of accounts
+        // if ans = 1, display list of budgets
     }
 
 }
