4ff3f8aaa3c5450d1e663b483a4b37402dd076bd
cgwheeler96 <wheelerchristopherg@gmail.com>
2018-04-26 11:28:41 -0500

    added factory methods

--- main/graphsubsys/Graph.java
+++ main/graphsubsys/Graph.java
@@ -1,7 +1,13 @@
 package main.graphsubsys;
 
 import javax.swing.JPanel;
+import java.awt.geom.Point2D;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 
 public class Graph extends JPanel {
-    
+    public Graph(String type, double[] x, double[] y) {
+        
+    }
 }
--- main/graphsubsys/GraphFactory.java
+++ main/graphsubsys/GraphFactory.java
@@ -1,13 +1,26 @@
 package main.graphsubsys;
 
-import java.awt.geom.Point2D;
-
 public class GraphFactory {
-    public static Graph createLineGraph(double[] x, double[] y) {
-        Graph lineGraph = new Graph("lineGraph", double[] x, double[] y);
+    public static Graph createLineGraph(double[] x, double[] y, String xaxisLabel, String yaxisLabel) {
+        Graph lineGraph = new Graph("line graph", x, y);
+        lineGraph.addLabels(xaxisLabel, yaxisLabel);
+        return lineGraph;
     }
     
-    public static Graph createBarGraph() {
-        
+    public static Graph createBarGraph(double[] x, double[] y, String xaxisLabel, String yaxisLabel) {
+        Graph barGraph = new Graph("bar graph", x, y);
+        barGraph.addLabels(xaxisLabel, yaxisLabel);
+        return barGraph;
+    }
+    
+    public static Graph createPieChart(double[] percentages, String[] labels) {
+        Graph pieChart = new Graph(percentages, labels);
+        return pieChart;
+    }
+    
+    public static Graph createAmortizationCalendar(double maxHeight, double[] stepHeights, String xaxisLabel, String yaxisLabel) {
+        Graph amCal = new Graph(maxHeight, stepHeights);
+        amCal.addLabels(xaxisLabel, yaxisLabel);
+        return amCal;
     }
 }
