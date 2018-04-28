86b5cca16fcd09f407331700fb1d4bc7d06fb290
brodderick rodriguez <bcr@auburn.edu>
2018-04-27 19:24:09 -0500

    Working on filter

--- main/userinterface/FilterForm.java
+++ main/userinterface/FilterForm.java
@@ -15,7 +15,6 @@ public class FilterForm extends Form {
         super(parent);
 
         setGridLayout(6, 3);
-
         addLabel("Filter Transactions");
         addTextField("accountName", "Enter Account Name");
         addTextArea("TransactionList",100,1,false);
@@ -27,11 +26,11 @@ public class FilterForm extends Form {
         addButton("f_byCategory", "Filter By Category");
         addPlaceholder();
         addPlaceholder();
-        addPlaceholder();
+        addButton("f_byCategoryDate", "Filter By Date and Category");
         addButton("back", "Back");
 
-        String all = transactionSystem.getAllTransactionsString();
-        this.setText("TransactionList", all);
+    //    String all = transactionSystem.getAllTransactionsString();
+    //    this.setText("TransactionList", all);
     }
 
     public void actionPerformed(ActionEvent event) {
@@ -47,12 +46,26 @@ public class FilterForm extends Form {
                     Date date1 = format.parse(getTextFromInput("startDate"));
                     Date date2 = format.parse(getTextFromInput("endDate"));
                     transactionSystem.filterController.FilterByDate(getTextFromInput("accountName"), date1, date2);
+                    transactionSystem.filterController.DisplayFilteredTransactions(this, "TransactionList");
+                    System.out.println("f_byDate");
                 } catch (ParseException e) { }
                 break;
 
             case "f_byCategory":
                 transactionSystem.filterController.FilterByCategory(getTextFromInput("accountName"), getTextFromInput("category"));
                 transactionSystem.filterController.DisplayFilteredTransactions(this, "TransactionList");
+                System.out.println("f_byCategory");
+                break;
+
+            case "f_byCategoryDate":
+                try {
+                    DateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+                    Date date1 = format.parse(getTextFromInput("startDate"));
+                    Date date2 = format.parse(getTextFromInput("endDate"));
+                    transactionSystem.filterController.FilterByCategoryAndDate(getTextFromInput("accountName"), getTextFromInput("category"), date1, date2);
+                    transactionSystem.filterController.DisplayFilteredTransactions(this, "TransactionList");
+                    System.out.println("f_byCategoryDate");
+                } catch (ParseException e) { }
                 break;
         }
     }
