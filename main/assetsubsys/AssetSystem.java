62757d89be153a806a86d2fd44a0fc5fcc63b751
brodderick rodriguez <bcr@auburn.edu>
2018-04-29 14:12:27 -0500

    Moved asset parsing to AssetSubsystem, updated DisplayValueOfAssets to grab from repo

--- main/assetsubsys/AssetSystem.java
+++ main/assetsubsys/AssetSystem.java
@@ -1,9 +1,19 @@
 package main.assetsubsys;
 
 import java.util.Date;
+import main.repositorysys.Repository;
+
+import java.util.*;
+import java.io.*;
+
 import main.userinterface.SavingsNetworthValueForm;
 import main.userinterface.AmortizationCalendarForm;
 import main.userinterface.Form;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Scanner;
+
 
 public class AssetSystem {
 
@@ -14,17 +24,36 @@ public class AssetSystem {
     public static void createSavingsNetWorthValueController(String userChoice, Date startDate, SavingsNetworthValueForm formIn) {
         savNetWorthController = new SavingsNetWorthValueController(userChoice, startDate, formIn);
     }
-    
+
     public static void createAmCalController(AmortizationCalendarForm formIn, String loanNameIn) {
         amCalController = new AmCalController(formIn, loanNameIn);
     }
-    
+
     public static void createNewAssetController(Form form) {
         newAssetController = new NewAssetController(form);
     }
-    
+
     public static NewAssetController getNewAssetController() {
         return newAssetController;
     }
+
+    public static void parseAssetCSV() {
+        String line = "";
+        try (BufferedReader br = new BufferedReader(new FileReader("main/data/assets.csv"))) {
+            while ((line = br.readLine()) != null) {
+                String[] dat = line.split(",");
+                DateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+
+                try {
+                    Date date = format.parse(dat[3]);
+                    Repository.createAsset(dat[0], dat[1], Double.parseDouble(dat[2]), date);
+                 } catch (ParseException e) {
+                     System.out.println("Incorrect date format");
+                 } // catch
+            } // while
+        } catch (IOException e) {
+            e.printStackTrace();
+        } // catch
+    } // parseAssetCSV()
 
 }
--- main/assetsubsys/DisplayValueOfAssetsController.java
+++ main/assetsubsys/DisplayValueOfAssetsController.java
@@ -1,23 +1,13 @@
 package main.assetsubsys;
 import main.userinterface.Form;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.text.ParseException;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
 import java.text.DecimalFormat;
 import java.awt.Dimension;
 import java.awt.Font;
 import javax.swing.plaf.FontUIResource;
+import main.repositorysys.Asset;
+import main.repositorysys.Repository;
 
 public class DisplayValueOfAssetsController {
 
@@ -26,22 +16,14 @@ public class DisplayValueOfAssetsController {
         double assetSum = 0;
         String line = "";
 
-        try (BufferedReader br = new BufferedReader(new FileReader("main/data/assets.csv"))) {
-            while ((line = br.readLine()) != null) {
-                String[] rmdline = line.split(",");
-                assetSum += Double.parseDouble(rmdline[2]);
-            } // while
-        } catch (IOException e) {
-            e.printStackTrace();
-        } // catch
+        for (Asset a : Repository.getAssets())
+            assetSum += a.getValue();
 
-        DecimalFormat formatter = new DecimalFormat("###,###,###.00");
-        
-        //Clean up font, text size, etc.
         UIManager.put("OptionPane.minimumSize",new Dimension(800,300));
         UIManager.put("OptionPane.messageFont", new Font("Arial", Font.PLAIN, 40));
         UIManager.put("OptionPane.buttonFont", new FontUIResource(new Font("Arial",Font.PLAIN,35)));
-        
+
+        DecimalFormat formatter = new DecimalFormat("###,###,###.00");
         JOptionPane.showMessageDialog(form, "Value of assets: $"+ formatter.format(assetSum));
 
     } // DisplayValueOfAssetsController()
